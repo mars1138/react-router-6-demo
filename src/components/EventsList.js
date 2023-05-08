29232ce562424aa436cc7339d9089409e7343fb2
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-05-07 22:06:17 -0700

    update

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,6 +1,11 @@
+// import { useLoaderData } from 'react-router-dom';
+
 import classes from './EventsList.module.css';
 
 function EventsList({ events }) {
+  // can use loader() data at this level, if desired
+  //   const events = useLoaderData();
+
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
