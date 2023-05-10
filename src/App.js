440f08b0f6e83741bd6ec12b07d48c182f24adf6
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-05-09 21:42:30 -0700

    delete event

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import EventsRootLayout from './pages/EventsRoot';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from './pages/EventDetail';
 import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: 'edit', element: <EditEventPage /> },
             ],
